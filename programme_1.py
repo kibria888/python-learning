2e55324cf9e7aab7fe52936512710b520c300932
kibria888 <gmkibria888@gmail.com>
2025-06-04 11:57:01 +0600

    not getting it

---
+++ programme_1.py
@@ -0,0 +1,14 @@
+s=print("hello world")
+print("how are you ?")
+print(123.34)
+if (3>4):
+    print("your name is kibria")
+print("i am not kibria")
+a="kibria"
+b="12403018"
+c=4.95
+print(type(a))
+print(type(b))
+d=round(c)
+print(s)
+print(type(s))
